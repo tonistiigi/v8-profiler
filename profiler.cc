b8967de09c0a6e343b2b164af6d5197fccbb8495
Tõnis Tiigi <tonistiigi@gmail.com>
2013-03-13 12:52:37 +0200

    Remove heap profiler. Quick fix for Node v0.10

--- profiler.cc
+++ profiler.cc
@@ -1,10 +1,10 @@
-#include "heap_profiler.h"
+//#include "heap_profiler.h"
 #include "cpu_profiler.h"
 
 namespace nodex {
   void InitializeProfiler(Handle<Object> target) {
     HandleScope scope;
-    HeapProfiler::Initialize(target);
+    //HeapProfiler::Initialize(target);
     CpuProfiler::Initialize(target);
   }
 
